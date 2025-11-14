2114a482db52966c58d1d4597745821fbc945afc
dhisa <ijlalwindhi15@gmail.com>
2025-11-14 19:34:07 +0700

    feat(types): add role types and IRoleStore interface

---
+++ types/role.ts
@@ -0,0 +1,8 @@
+export interface IRoleStore {
+  address: string | null;
+  role: TRole | null;
+  setAddress: (addr: string | null) => void;
+  setRole: (role: TRole | null) => void;
+}
+
+export type TRole = "SUPER_ADMIN" | "ADMIN" | "USER";
