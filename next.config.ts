516f035ffe99992622af8d8a4597cb43ab9b9b39
dhisa <ijlalwindhi15@gmail.com>
2025-11-23 09:01:41 +0700

    feat(config): add image domain to next.config for external image support

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@mysten/walrus", "@mysten/walrus-wasm"],
+  images: { domains: ["avatar.vercel.sh"] },
 };
 
 export default nextConfig;
