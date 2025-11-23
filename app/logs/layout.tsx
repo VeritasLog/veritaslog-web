4ff4be4b99d9f46546a0b46e7282cc35b73b8d18
dhisa <ijlalwindhi15@gmail.com>
2025-11-23 08:44:59 +0700

    feat(logs-layout): create LogsLayout component with Navbar and SuiProviders

---
+++ app/logs/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/layout/navbar";
+import { SuiProviders } from "@/lib/sui-provider";
+
+function LogsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SuiProviders>
+      <main className="p-6 space-y-6">
+        <Navbar />
+        {children}
+      </main>
+    </SuiProviders>
+  );
+}
+
+export default LogsLayout;
